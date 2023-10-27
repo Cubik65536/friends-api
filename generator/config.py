1cacb12e221d4795fb9bdeeaf39635ca29087e48
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2023-10-26 20:00:15 -0400

    feat: Enable friends groups

--- generator/config.py
+++ generator/config.py
@@ -2,7 +2,7 @@
 import yaml
 
 def load():
-    f = open('config.yml', 'r',encoding='utf-8')
+    f = open('config.yml', 'r', encoding='utf-8')
     ystr = f.read()
     ymllist = yaml.load(ystr, Loader=yaml.FullLoader)
     return ymllist
